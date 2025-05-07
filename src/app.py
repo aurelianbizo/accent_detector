a31526ecf5464b54d8c84b77e0a359ddc044e87a
Aurelian <aurelian.bizo@gmail.com>
2025-05-07 18:13:30 +0300

    adding accuracy

--- src/app.py
+++ src/app.py
@@ -60,15 +60,30 @@ def transcribe_audio(audio_file):
 
 # Step 2: Use OpenAI to classify accent
 def classify_accent(text):
-    prompt = f"Based on the following text, classify the speaker's accent: {text}. The options are British, American, Australian, Canadian, Irish, or other accents."
+    prompt = f"Based on the following text, classify the speaker's accent: {text}. The options are British, American, Australian, Canadian, Irish, or other accents. Please provide also the accuracy of the classification in the form of accuracy=0.XX. You should always use equal sign(=) for the accuracy."
     
     response = openai.chat.completions.create(
         model="gpt-4o-mini",
         messages=[{'role': 'user', 'content': prompt}]
     )
     accent = response.choices[0].message.content
+    if 'British' in accent:
+        single_accent = 'British'
+    elif 'American' in accent:
+        single_accent = 'American'
+    elif 'Australian' in accent:
+        single_accent = 'Australian'
+    elif 'Canadian' in accent:
+        single_accent = 'Canadian'
+    elif 'Irish' in accent:
+        single_accent = 'Irish'
+    else:
+        single_accent = 'Other'
+    
+    accuracy = accent.split('=')[-1].strip()[:4]
+    #accent = accent.split('accuracy=')[0].strip()
     print(f"Classified Accent: {accent}")
-    return accent
+    return accent, single_accent, accuracy
 
 @app.route('/')
 def index():
@@ -88,8 +103,8 @@ def process_submission():
             transcription = transcribe_audio(file_path)
             
             if transcription:
-                accent = classify_accent(transcription)
-                return render_template('result.html', transcription=transcription, accent=accent)
+                accent, single_accent, accuracy = classify_accent(transcription)
+                return render_template('result.html', transcription=transcription, accent=accent, single_accent=single_accent, accuracy=accuracy)
             else:
                 return jsonify({'error': 'Unable to transcribe the audio.'}), 400
         elif file.filename.endswith('.mp4'):
@@ -99,27 +114,12 @@ def process_submission():
             transcription = transcribe_audio(audio_file)
             
             if transcription:
-                accent = classify_accent(transcription)
-                return render_template('result.html', transcription=transcription, accent=accent)
+                accent, single_accent, accuracy = classify_accent(transcription)
+                return render_template('result.html', transcription=transcription, accent=accent, single_accent=single_accent, accuracy=accuracy)
             else:
                 return jsonify({'error': 'Unable to transcribe the audio from the video.'}), 400
         else:
             return jsonify({'error': 'Only WAV or MP4 files are allowed'}), 400
-        # if file.filename.endswith('.wav'):
-        #     file_path = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
-        #     file.save(file_path)
-        #     print(f"File saved at: {file_path}")
-
-        #     # Transcribe the audio file
-        #     transcription = transcribe_audio(file_path)
-            
-        #     if transcription:
-        #         accent = classify_accent(transcription)
-        #         return render_template('result.html', transcription=transcription, accent=accent)
-        #     else:
-        #         return jsonify({'error': 'Unable to transcribe the audio.'}), 400
-        # else:
-        #     return jsonify({'error': 'Only WAV files are allowed'}), 400
 
     # Check if the form contains a URL
     elif 'url' in request.form and request.form['url']:
@@ -137,93 +137,12 @@ def process_submission():
         transcription = transcribe_audio('downloaded_audio.wav')
         
         if transcription:
-            accent = classify_accent(transcription)
-            return render_template('result.html', transcription=transcription, accent=accent)
+            accent, single_accent, accuracy = classify_accent(transcription)
+            return render_template('result.html', transcription=transcription, accent=accent, single_accent=single_accent, accuracy=accuracy)
         else:
             return jsonify({'error': 'Unable to transcribe the audio from the video.'}), 400
     else:
         return jsonify({'error': 'No file or URL provided'}), 400
-# @app.route('/process', methods=['POST'])
-# def process_file():
-#     if 'file' not in request.files:
-#         return jsonify({'error': 'No file part'}), 400
-    
-#     file = request.files['file']
-    
-#     if file.filename == '':
-#         return jsonify({'error': 'No selected file'}), 400
-    
-#     if file and file.filename.endswith('.wav'):
-#         # Save the uploaded file
-#         file_path = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
-#         file.save(file_path)
-#         print(f"File saved at: {file_path}")
-        
-#         # Transcribe the audio file
-#         transcription = transcribe_audio(file_path)
-        
-#         if transcription:
-#             # Classify the accent based on the transcription
-#             accent = classify_accent(transcription)
-            
-#             if accent:
-#                 return render_template('result.html', transcription=transcription, accent=accent)
-#             else:
-#                 return jsonify({'error': 'Unable to classify the accent.'}), 500
-#         else:
-#             return jsonify({'error': 'Unable to transcribe the audio.'}), 400
-#     else:
-#         return jsonify({'error': 'Only WAV files are allowed'}), 400
-
-# @app.route('/process', methods=['POST'])
-# def process_url():
-#     # Get URL from form input
-#     url = request.form.get('url')
-    
-#     if url:
-#         # Here you can process the URL (e.g., validate it or use it for further actions)
-#         # For simplicity, we'll just return the URL back to the user.
-#         return render_template('result.html', url=url)
-#     else:
-#         return "No URL provided. Please go back and enter a URL."
-# Step 3: Define Flask route to handle file upload and processing
-# @app.route('/upload', methods=['POST'])
-# def upload_file():
-#     try:
-#         if 'file' not in request.files:
-#             return jsonify({'error': 'No file part'}), 400
-        
-#         file = request.files['file']
-        
-#         if file.filename == '':
-#             return jsonify({'error': 'No selected file'}), 400
-        
-#         # Save file to disk
-#         file_path = 'uploaded_audio.wav'
-#         file.save(file_path)
-#         print(f"File saved at: {file_path}")
-        
-#         # Transcribe the audio
-#         transcription = transcribe_audio(file_path)
-        
-#         if transcription:
-#             # Classify the accent based on the transcription
-#             accent = classify_accent(transcription)
-            
-#             if accent:
-#                 # Return the transcription and classified accent as JSON response
-#                 return jsonify({
-#                     'transcription': transcription,
-#                     'accent': accent
-#                 })
-#             else:
-#                 return jsonify({'error': 'Unable to classify the accent.'}), 500
-#         else:
-#             return jsonify({'error': 'Unable to transcribe the audio.'}), 400
-
-#     except Exception as e:
-#         print(f"Error during file processing: {e}")
-#         return jsonify({'error': 'Internal server error'}), 500
 
 
 # Step 4: Run the Flask app
